8c8e45521ae085e2380a584eba2f12c242481104
koolmaster <koolmaster1408@gmail.com>
2018-06-19 17:48:34 +0700

    update project , add interest , add contact ,update img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { EasyPieChartModule } from 'ng2modules-easypiechart';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 //#region  -- KHAI BÁO COMPONENT --
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
@@ -11,6 +14,33 @@ import { ExperienceComponent } from './portfolio/experience/experience.component
 import { SkillComponent } from './portfolio/skill/skill.component';
 import { EducationComponent } from './portfolio/education/education.component';
 import { ProjectComponent } from './portfolio/project/project.component';
+import { InterestComponent } from './portfolio/interest/interest.component';
+import { ContactComponent } from './portfolio/contact/contact.component';
+
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  spaceBetween: 30,
+  slidesPerView: 6,
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    }
+  }
+};
 
 @NgModule({
   declarations: [
@@ -20,15 +50,23 @@ import { ProjectComponent } from './portfolio/project/project.component';
     ExperienceComponent,
     SkillComponent,
     EducationComponent,
-    ProjectComponent
+    ProjectComponent,
+    InterestComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFontAwesomeModule,
-    EasyPieChartModule
+    EasyPieChartModule,
+    SwiperModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
