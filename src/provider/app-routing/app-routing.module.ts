eca7af13e69a579c0488770849ec316bbfb3faaa
koolmaster <koolmaster1408@gmail.com>
2018-06-29 17:34:05 +0700

    add navbar admin, add header admin

--- src/provider/app-routing/app-routing.module.ts
+++ src/provider/app-routing/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: 'admin',
     component: AdminComponent,
     children: [
-      { path: '', redirectTo: 'login' , pathMatch: 'full'},
+      // { path: '', redirectTo: 'login' , pathMatch: 'full'},
       { path: 'login', component: LoginComponent },
     ]
   },
